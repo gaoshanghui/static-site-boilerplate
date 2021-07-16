2746c49d50df875b60f166321a0ba9ac72f0284f
Gao Shanghui <shanghui0109@gmail.com>
2021-07-16 18:50:49 +0900

    update the intro page with web animation

--- src/js/main.js
+++ src/js/main.js
@@ -5,64 +5,134 @@ import renderApp from './react-app/index';
 if (document.getElementById('react-app-root')) renderApp();
 
 
-/* Demo Page */
-// Text animation
-// if (document.querySelector('.Presentation')) {
-//   import('./utils/animations')
-//     .then((module) => {
-//       module.default();
-//     });
-// }
+/* Intro Page */
 
-gsap.registerPlugin(CSSRulePlugin);
-
-if (document.querySelector('.Presentation')) {
-  // GSAP Animation
-  // Loading GSAP plugins
-
-  // Texts animation
-  // const triggerAnimation = () => {
-  //   const rule1 = CSSRulePlugin.getRule('.Presentation__headline-text:after');
-  //   const rule2 = CSSRulePlugin.getRule('.Presentation__version-info:after');
-  //   const rule3 = CSSRulePlugin.getRule('.Presentation__special-message:after');
-
-  //   const timeLine = gsap.timeline({ delay: 1 });
-
-  //   timeLine
-  //     .to('.Presentation__section-line', { duration: 3.5, scale: 1, transformOrigin: "50% 50%", ease: Power4.easeOut, delay: 1 })
-  //     .to(rule1, { duration: 1, cssRule: { scaleY: 0 } }, "-=1")
-  //     .to(rule2, { duration: 1, cssRule: { scaleY: 0 } })
-  //     .to(rule3, { duration: 1, cssRule: { scaleY: 0 } }, "-=0.5");
-  // }
-
-  // triggerAnimation();
-
-  const timeLine = gsap.timeline({ 
+// Web Animation
+if (document.querySelector('.intro-web-animation')) {
+  // Main timeline, other timelines will be added into this one.
+  const timelineMain = gsap.timeline({
     delay: 1,
-    yoyo:true, 
+    yoyo: true,
     repeat: -1,
     repeatDelay: 2
   });
 
-  // gsap.set('#source-code-container-artboard', {});
 
-  // Device Animation
-  timeLine
-    .addLabel('deviceFrame')
-    .from('#outter', { duration: 2, opacity: 0, transformOrigin: "0 0", scale: 1 }, 'deviceFrame')
-    .from('#inner', { duration: 2, opacity: 0, transformOrigin: "0 0",  scale: 1 }, 'deviceFrame')
-    .from('#middle', { duration: 2, opacity: 0, transformOrigin: "0 100",  scale: 1 }, 'deviceFrame')
-    .addLabel('lightEffect')
-    .from('#spotlight', { duration: 2, opacity: 0, transformOrigin: "0 0", scale: 1 }, 'lightEffect')
-    .from('#screen-reflection', { duration: 2, opacity: 0, transformOrigin: "0 0", scale: 1 }, 'lightEffect')
-    .from('#content-background', { duration: 1, opacity: 0, transformOrigin: "0 0", scaleY: 0 })
-    .from('#image-background', { duration: 1, opacity: 0, transformOrigin: "0 0", scaleY: 0 })
-    .from('#cross-lines', { duration: 1, transformOrigin: "50% 50%", scale: 0 })
-    .from('#headline', { duration: 1, opacity: 0, transformOrigin: "0 100%", scaleX: 0 })
-    .from('#paragraph', { duration: 1, opacity: 0, transformOrigin: "0 100%", scaleX: 0 })
-    .from('#button', { duration: 1, opacity: 0, transformOrigin: "0 100%", scaleX: 0 })
-    // source-code-container
-    .addLabel('sourcecode')
-    .fromTo('#source-code-container-artboard', {  opacity: 0, x: 50, rotationY: "90"}, { duration: 5, x: -200, opacity: 1, rotationY: '0'}, 'sourcecode')
-    .from('#source-code', { duration: 2, opacity: 0, width: 0}, 'sourcecode')
+  // Timeline creater
+  // animate device frame
+  function timelineDeviceFrame() {
+    const outerDeviceFrame = document.querySelector('#outer');
+    const outerDeviceFrameLength = outerDeviceFrame.getTotalLength();
+    const innerDeviceFrame = document.querySelector('#inner');
+    const innerDeviceFrameLength = innerDeviceFrame.getTotalLength();
+    const middelDeviceFrame = document.querySelector('#middle');
+
+    const contentBackground = document.querySelector('#content-background');
+    const spotlight = document.querySelector('#spotlight');
+    const screenReflection = document.querySelector('#screen-reflection');
+
+    const tl = gsap.timeline();
+
+    tl
+      .addLabel('drawDeviceFrameSameTime')
+      .fromTo(outerDeviceFrame, { strokeDasharray: outerDeviceFrameLength, strokeDashoffset: outerDeviceFrameLength, opacity: 0, transformOrigin: '50% 50%', rotation: 180, fill: 'none' }, { duration: 1.5, ease: Power4.easeOut, transformOrigin: '50% 50%', strokeDasharray: outerDeviceFrameLength, strokeDashoffset: 0, opacity: 1, fill: '#14212E' }, 'drawDeviceFrameSameTime')
+      .fromTo(innerDeviceFrame, { strokeDasharray: innerDeviceFrameLength, strokeDashoffset: innerDeviceFrameLength, opacity: 0 }, { duration: 1.5, ease: Power4.easeOut, strokeDasharray: innerDeviceFrameLength, strokeDashoffset: 0, opacity: 1 }, 'drawDeviceFrameSameTime')
+      .from(middelDeviceFrame, { duration: 1, ease: Power4.easeOut, opacity: 0, }, 'drawDeviceFrameSameTime+=0.5')
+      .from(contentBackground, { duration: 1, opacity: 0, height: 0 }, 'drawDeviceFrameSameTime+=0.5')
+      .addLabel('animateLightAndReflection')
+      .from(spotlight, { duration: 1, opacity: 0, height: 0 }, 'animateLightAndReflection')
+      .from(screenReflection, { duration: 1, opacity: 0 }, 'animateLightAndReflection' )
+
+    return tl;
+  }
+
+  // Timeline creater
+  // animate source code and app rendering
+  function timelineSourceCode() {
+    // source code SVG part
+    const codeFunctionOpen = document.querySelector('#function-open');
+    const codeFunctionClose = document.querySelector('#function-close');
+    const codeFunctionArticle = document.querySelector('#article-part');
+    const codeFunctionButton = document.querySelector('#button-part');
+    
+    // app content part
+    const appImageBackground = document.querySelector('#image-background');
+    const appImageCrossLines = document.querySelector('#cross-lines');
+    const appHeadline = document.querySelector('#headline');
+    const appParagraph = document.querySelector('#paragraph');
+    const appButton = document.querySelector('#button');
+
+    const tl = gsap.timeline();
+    
+    tl
+      .addLabel('animateFunctionContainer')
+      .fromTo(codeFunctionOpen, { opacity: 0, y: 10 }, {duration: 1, ease: Power4.easeOut, opacity: 1, y: 0}, 'animateFunctionContainer')
+      .fromTo(codeFunctionClose, { opacity: 0, y: -80 }, {duration: 1, ease: Power4.easeOut, opacity: 1, y: -90}, 'animateFunctionContainer')
+      .from(appImageBackground, { duration: 0.5, opacity: 0, height: 0 }, '-=0.5')
+      .from(appImageCrossLines, { duration: 0.5, opacity: 0, transformOrigin: "50% 50%", scale: 0 })
+      .fromTo(codeFunctionClose, { y: -90 }, {duration: 0.5, ease: Power4.easeOut, y: -20})
+      .fromTo(codeFunctionArticle, { opacity: 0, y: 10 }, {duration: 1, ease: Power4.easeOut, opacity: 1, y: 0})
+      .addLabel('animateAppArticle')
+      .from(appHeadline, { duration: 1, ease: Power4.easeOut, transformOrigin: "0 100%", scaleX: 0 }, 'animateAppArticle-=0.5')
+      .from(appParagraph, { duration: 1, ease: Power4.easeOut, transformOrigin: "0 100%", scaleX: 0 }, 'animateAppArticle+=0.4')
+      .fromTo(codeFunctionClose, { y: -20 }, {duration: 0.5, ease: Power4.easeOut, y: 0})
+      .fromTo(codeFunctionButton, { opacity: 0, y: 10 }, {duration: 1, ease: Power4.easeOut, opacity: 1, y: 0})
+      .from(appButton, { duration: 1, ease: Power4.easeOut, transformOrigin: "0 100%", scaleX: 0 }, '-=0.5')
+
+    return tl;
+  }
+
+  // Timeline creater
+  function timelineSourceCodeCanvas() {
+    const sourcecodeCanvas = document.querySelector('#source-code-canvas');
+
+    const tl = gsap.timeline();
+    tl
+      .fromTo(sourcecodeCanvas, { scale: 0.6, opacity: 0, x:400, rotationY: "90" }, { scale: 1, duration: 3, ease: Power4.easeOut, opacity: 1, x:0, rotationY: '0' })
+
+    return tl;
+  }
+
+  // Timeline creater
+  // Basic Shapes animation timeline
+  function timelineBasicShapes() {
+    const triangleCanvas = document.querySelector('#triangle-canvas');
+    const squareCanvas = document.querySelector('#square-canvas');
+    const ellipseCanvas = document.querySelector('#ellipse-canvas');
+
+    const tl = gsap.timeline()
+    tl
+    .fromTo(ellipseCanvas, { opacity: 0, rotationY: "90" }, { duration: 1, ease: Power4.easeOut, opacity: 1, rotationY: '0'})
+    .fromTo(squareCanvas, { opacity: 0, rotationY: "90" }, { duration: 1, ease: Power4.easeOut, opacity: 1, rotationY: '0'}, '-=0.5')
+    .fromTo(triangleCanvas, { opacity: 0, rotationY: "90" }, { duration: 1, ease: Power4.easeOut, opacity: 1, rotationY: '0'}, '-=0.5')
+
+    return tl;
+  }
+
+  // Timeline creater
+  // Texts animation timeline
+  function timelineTexts () {
+    const largeTextLine1 = document.querySelector('#large-text-line1');
+    const largeTextLine2 = document.querySelector('#large-text-line2');
+    const smallTextLine1 = document.querySelector('#small-text-line1');
+    const smallTextLine2 = document.querySelector('#small-text-line2');
+
+    const tl = gsap.timeline()
+    tl
+      .from(largeTextLine1, {duration: 2, opacity: 0, ease: Power4.easeOut, y: 50})
+      .from(largeTextLine2, {duration: 2, opacity: 0, ease: Power4.easeOut, y: 50}, '-=1.8')
+      .from(smallTextLine1, {duration: 2, opacity: 0, ease: Power4.easeOut, y: 50}, '-=1.8')
+      .from(smallTextLine2, {duration: 2, opacity: 0, ease: Power4.easeOut, y: 50}, '-=1.8')
+
+    return tl;
+  }
+
+  timelineMain
+    .addLabel('startAtSameTime')
+    .add(timelineDeviceFrame(), 'startAtSameTime')
+    .add(timelineBasicShapes(), 'startAtSameTime')
+    .add(timelineTexts(), 'startAtSameTime')
+    .add(timelineSourceCodeCanvas(), 'startAtSameTime+=0.5')
+    .add(timelineSourceCode(), "-=1.7")
+
 };
